6b395fe23263370fe1d99d1bd0290abed0892354
James Robinson <james.em.robinson@gmail.com>
2023-10-02 15:13:34 +0100

    :goal_net: Catch initialisation errors caused by incorrect variable setting

--- run.py
+++ run.py
@@ -4,21 +4,28 @@ from apricot import ApricotServer
 from apricot.oauth import OAuthBackend
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(
-        prog="Apricot",
-        description="Apricot is a proxy for delegating LDAP requests to an OpenID Connect backend.",
-    )
-    # Common options needed for all backends
-    parser.add_argument("-b", "--backend", type=OAuthBackend, help="Which OAuth backend to use.")
-    parser.add_argument("-d", "--domain", type=str, help="Which domain users belong to.")
-    parser.add_argument("-p", "--port", type=int, default=8080, help="Port to run on.")
-    parser.add_argument("-i", "--client-id", type=str, help="OAuth client ID.")
-    parser.add_argument("-s", "--client-secret", type=str, help="OAuth client secret.")
-    # Options for Microsoft Entra backend
-    parser.add_argument("-t", "--entra-tenant-id", type=str, help="Microsoft Entra tenant ID.", required=False)
-    # Parse arguments
-    args = parser.parse_args()
+    try:
+        parser = argparse.ArgumentParser(
+            prog="Apricot",
+            description="Apricot is a proxy for delegating LDAP requests to an OpenID Connect backend.",
+        )
+        # Common options needed for all backends
+        parser.add_argument("-b", "--backend", type=OAuthBackend, help="Which OAuth backend to use.")
+        parser.add_argument("-d", "--domain", type=str, help="Which domain users belong to.")
+        parser.add_argument("-p", "--port", type=int, default=8080, help="Port to run on.")
+        parser.add_argument("-i", "--client-id", type=str, help="OAuth client ID.")
+        parser.add_argument("-s", "--client-secret", type=str, help="OAuth client secret.")
+        # Options for Microsoft Entra backend
+        group = parser.add_argument_group("Microsoft Entra")
+        group.add_argument("-t", "--entra-tenant-id", type=str, help="Microsoft Entra tenant ID.", required=False)
+        # Parse arguments
+        args = parser.parse_args()
 
-    # Create the Apricot server
-    reactor = ApricotServer(**vars(args))
+        # Create the Apricot server
+        reactor = ApricotServer(**vars(args))
+    except Exception:
+        msg = "Unable to initialise Apricot server from provided command line arguments."
+        raise ValueError(msg)
+
+    # Run the Apricot server
     reactor.run()
