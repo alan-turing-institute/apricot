fd1eb33c53ec49408de69bee7edaa8d000e08d84
James Robinson <james.em.robinson@gmail.com>
2024-02-26 12:38:40 +0000

    :rotating_light: Fix linting errors

--- apricot/patches/__init__.py
+++ apricot/patches/__init__.py
@@ -1,15 +1,5 @@
-"""Patch LDAPString to avoid TypeError when parsing LDAP filter strings"""
+from .ldap_string import LDAPString  # type: ignore[attr-defined]
 
-from ldaptor.protocols.pureldap import LDAPString
-
-old_init = LDAPString.__init__
-
-
-def patched_init(self, *k, **kw):
-    """Patch LDAPString init to store its value as 'str' not 'bytes'"""
-    old_init(self, *k, **kw)
-    if isinstance(self.value, bytes):
-        self.value = self.value.decode()
-
-
-LDAPString.__init__ = patched_init
+__all__ = [
+    "LDAPString",
+]
