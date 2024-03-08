1363af40debe82a165ed302569090575049224a5
James Robinson <james.em.robinson@gmail.com>
2024-03-08 15:26:46 +0000

    :loud_sound: Add debug messages to ReadOnlyLDAPServer handlers

--- apricot/ldap/read_only_ldap_server.py
+++ apricot/ldap/read_only_ldap_server.py
@@ -4,9 +4,18 @@ from ldaptor.interfaces import ILDAPEntry
 from ldaptor.protocols.ldap.ldaperrors import LDAPProtocolError
 from ldaptor.protocols.ldap.ldapserver import LDAPServer
 from ldaptor.protocols.pureldap import (
+    LDAPAddRequest,
     LDAPBindRequest,
+    LDAPCompareRequest,
+    LDAPDelRequest,
+    LDAPExtendedRequest,
+    LDAPModifyDNRequest,
+    LDAPModifyRequest,
+    LDAPProtocolRequest,
+    LDAPSearchRequest,
     LDAPSearchResultDone,
     LDAPSearchResultEntry,
+    LDAPUnbindRequest,
 )
 from twisted.internet import defer
 from twisted.python import log
@@ -21,19 +30,19 @@ class ReadOnlyLDAPServer(LDAPServer):
 
     def getRootDSE(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPProtocolRequest,
         reply: Callable[[LDAPSearchResultEntry], None] | None,
     ) -> LDAPSearchResultDone:
         """
-        Handle an LDAP Root RSE request
+        Handle an LDAP Root DSE request
         """
         if self.debug:
-            log.msg("Handling an LDAP Root RSE request")
+            log.msg(f"Handling an LDAP Root DSE request.")
         return super().getRootDSE(request, reply)
 
     def handle_LDAPAddRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPAddRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[..., None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -41,7 +50,7 @@ class ReadOnlyLDAPServer(LDAPServer):
         Refuse to handle an LDAP add request
         """
         if self.debug:
-            log.msg("Handling an LDAP add request")
+            log.msg(f"Handling an LDAP add request.")
         id((request, controls, reply))  # ignore unused arguments
         msg = "ReadOnlyLDAPServer will not handle LDAP add requests"
         raise LDAPProtocolError(msg)
@@ -56,12 +65,12 @@ class ReadOnlyLDAPServer(LDAPServer):
         Handle an LDAP bind request
         """
         if self.debug:
-            log.msg(f"Handling an LDAP bind request")
+            log.msg(f"Handling an LDAP bind request.")
         return super().handle_LDAPBindRequest(request, controls, reply)
 
     def handle_LDAPCompareRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPCompareRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[..., None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -69,12 +78,12 @@ class ReadOnlyLDAPServer(LDAPServer):
         Handle an LDAP compare request
         """
         if self.debug:
-            log.msg("Handling an LDAP compare request")
+            log.msg(f"Handling an LDAP compare request.")
         return super().handle_LDAPCompareRequest(request, controls, reply)
 
     def handle_LDAPDelRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPDelRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[..., None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -82,14 +91,14 @@ class ReadOnlyLDAPServer(LDAPServer):
         Refuse to handle an LDAP delete request
         """
         if self.debug:
-            log.msg("Handling an LDAP delete request")
+            log.msg(f"Handling an LDAP delete request.")
         id((request, controls, reply))  # ignore unused arguments
         msg = "ReadOnlyLDAPServer will not handle LDAP delete requests"
         raise LDAPProtocolError(msg)
 
     def handle_LDAPExtendedRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPExtendedRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[..., None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -97,12 +106,12 @@ class ReadOnlyLDAPServer(LDAPServer):
         Handle an LDAP extended request
         """
         if self.debug:
-            log.msg("Handling an LDAP extended request")
+            log.msg(f"Handling an LDAP extended request.")
         return super().handle_LDAPExtendedRequest(request, controls, reply)
 
     def handle_LDAPModifyDNRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPModifyDNRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[..., None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -110,14 +119,14 @@ class ReadOnlyLDAPServer(LDAPServer):
         Refuse to handle an LDAP modify DN request
         """
         if self.debug:
-            log.msg("Handling an LDAP modify DN request")
+            log.msg(f"Handling an LDAP modify DN request.")
         id((request, controls, reply))  # ignore unused arguments
         msg = "ReadOnlyLDAPServer will not handle LDAP modify DN requests"
         raise LDAPProtocolError(msg)
 
     def handle_LDAPModifyRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPModifyRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[..., None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -125,27 +134,14 @@ class ReadOnlyLDAPServer(LDAPServer):
         Refuse to handle an LDAP modify request
         """
         if self.debug:
-            log.msg("Handling an LDAP modify request")
+            log.msg(f"Handling an LDAP modify request.")
         id((request, controls, reply))  # ignore unused arguments
         msg = "ReadOnlyLDAPServer will not handle LDAP modify requests"
         raise LDAPProtocolError(msg)
 
-    def handle_LDAPUnbindRequest(  # noqa: N802
-        self,
-        request: LDAPBindRequest,
-        controls: list[LDAPControlTuple] | None,
-        reply: Callable[..., None] | None,
-    ) -> None:
-        """
-        Handle an LDAP unbind request
-        """
-        if self.debug:
-            log.msg("Handling an LDAP unbind request")
-        super().handle_LDAPUnbindRequest(request, controls, reply)
-
     def handle_LDAPSearchRequest(  # noqa: N802
         self,
-        request: LDAPBindRequest,
+        request: LDAPSearchRequest,
         controls: list[LDAPControlTuple] | None,
         reply: Callable[[LDAPSearchResultEntry], None] | None,
     ) -> defer.Deferred[ILDAPEntry]:
@@ -153,5 +149,18 @@ class ReadOnlyLDAPServer(LDAPServer):
         Handle an LDAP search request
         """
         if self.debug:
-            log.msg("Handling an LDAP search request")
+            log.msg(f"Handling an LDAP search request.")
         return super().handle_LDAPSearchRequest(request, controls, reply)
+
+    def handle_LDAPUnbindRequest(  # noqa: N802
+        self,
+        request: LDAPUnbindRequest,
+        controls: list[LDAPControlTuple] | None,
+        reply: Callable[..., None] | None,
+    ) -> None:
+        """
+        Handle an LDAP unbind request
+        """
+        if self.debug:
+            log.msg(f"Handling an LDAP unbind request.")
+        super().handle_LDAPUnbindRequest(request, controls, reply)
